af41ee94a71e52360aed8451e0f1db12ed0c4fed
GP <ganesh.prasannah@oslash.com>
2021-06-25 10:18:07 +0530

    scripts: Try to use the semantic-release git plugin to commit the version bump right in the build step instead of during the package step.

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,6 +4,6 @@ set -eu
 
 for filename in "${@:2}"
 do
-    echo "Bumping version to $1 in file $filename"
+    echo "📝 Bumping version to $1 in file $filename"
     npm_config_yes=true npx dot-json $filename version $1
 done
