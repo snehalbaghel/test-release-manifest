9e3e4759470b034e30ab4e9cc4fb17855d24cb3f
GP <ganesh.prasannah@oslash.com>
2021-06-25 10:05:37 +0530

    scripts: Allow positional arguments for bump script.

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,7 +3,9 @@
 set -eu
 
 echo "Bumping version to $1"
-npm_config_yes=true npx dot-json package.json version $1
-npm_config_yes=true npx dot-json manifest.json version $1
-npm_config_yes=true npx dot-json build-output/package.json version $1
-npm_config_yes=true npx dot-json build-output/manifest.json version $1
+
+for filename in "${@:2}"
+do
+    echo "Setting version on $filename"
+    npm_config_yes=true npx dot-json $filename version $1
+done
